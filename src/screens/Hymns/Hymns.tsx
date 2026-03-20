2efacf9322e9d3320948bd7adb03487cba3abe63
Momen Mustafa <momenmustafa371@gmail.com>
2026-03-20 21:01:51 +0200

    refactor: make custom hookd and reusable components

--- src/screens/Hymns/Hymns.tsx
+++ src/screens/Hymns/Hymns.tsx
@@ -1,22 +1,15 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import AppView from '../../components/UI/AppView/AppView';
 import { styles } from './styles';
 import { LegendList, LegendListRenderItemProps } from '@legendapp/list';
 import { dummyHymns } from '../../utils/dummyData';
-import { useTranslation } from 'react-i18next';
 import { Hymn } from '../../firebase/models/hymnModel';
 import HymnItem from '../../components/HymnItem/HymnItem';
-import { ScreenHeader } from '../../components/ScreenHeader/ScreenHeader';
-import AppIcon from '../../components/UI/AppIcon/AppIcon';
-import { ICON_SIZES } from '../../designSystem/designSystem';
-import { LocalizationEnum } from '../../locales';
-import AppTextInput from '../../components/UI/AppTextInput/AppTextInput';
 import useHymnsListHandlers from '../../hooks/useHymnsListHandlers';
 import HymnsHeader from './components/HymnsHeader/HymnsHeader';
 
 const Hymns = () => {
   const [isGrid, setIsGrid] = useState(false);
-  const { t } = useTranslation();
 
   const { isRTL, onClearPress, onSearchPress, showSearch } =
     useHymnsListHandlers();
